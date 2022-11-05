8767b4fbacb30fd10c0af92bb4cd129890bf2a64
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-05 15:08:11 -0300

    feat(Header): create dropdown to account actions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,32 @@
+import { ReactNode, Children, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { ShoppingCart, User } from 'phosphor-react';
 
 import { useAuth } from '@hooks/useAuth';
 import * as Styled from './styles';
 
-// interface HeaderProps {}
+function Dropdown({ children }: { children: ReactNode }) {
+  const childrens = Children.toArray(children);
+
+  return (
+    <Styled.Dropdown>
+      {childrens.map((child) => (
+        <li>{child}</li>
+      ))}
+    </Styled.Dropdown>
+  );
+}
 
 export function Header() {
+  const [showUserDropdown, setShowUserDropdown] = useState(false);
+
   const { isAuthenticated, isLoading } = useAuth();
 
   const location = useLocation();
+
+  function handleUserButtonClick() {
+    setShowUserDropdown((prevState) => !prevState);
+  }
 
   return (
     <Styled.Container>
@@ -34,9 +51,17 @@ export function Header() {
               )}
 
               {isAuthenticated && (
-              <Styled.UserButton>
-                <User color="#fff" size={40} />
-              </Styled.UserButton>
+                <>
+                  <Styled.Button onClick={handleUserButtonClick}>
+                    <User color="#fff" size={40} />
+                  </Styled.Button>
+                  {showUserDropdown && (
+                    <Dropdown>
+                      <Link to="/">Assinaturas</Link>
+                      <Styled.Button>Sair</Styled.Button>
+                    </Dropdown>
+                  )}
+                </>
               )}
             </>
           )}
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -75,6 +75,8 @@ export const AccountIcons = styled.div`
   display: flex;
   align-items: center;
   gap: ${rem(24)};
+
+  position: relative;
 `;
 
 export const LoginButton = styled.button`
@@ -95,7 +97,72 @@ export const LoginButton = styled.button`
   }
 `;
 
-export const UserButton = styled.button`
+export const Button = styled.button`
+  font-size: inherit;
+  color: inherit;
   border: none;
   background-color: transparent;
+`;
+
+export const Dropdown = styled.ul`
+  list-style: none;
+
+  font-size: 1rem;
+  font-family: inherit;
+  color: #fff;
+
+  width: ${rem(180)};
+  background-color: #373737;
+  border-radius: 8px;
+  border-top-right-radius: 0;
+  padding: 0.5rem 1rem;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+
+  position: absolute;
+  z-index: 2;
+  bottom: 0;
+  right: 0;
+  transform: translateY(100%);
+
+  li {
+    position: relative;
+
+    &:not(:last-of-type) {
+      border-bottom: 2px solid rgba(255, 255, 255, 0.25);
+    }
+
+    &::after {
+      content: '';
+      display: block;
+      width: ${rem(180)};
+
+      position: absolute;
+      inset: 0;
+      left: -${rem(16)};
+
+      background-color: #373737;
+      z-index: -1;
+
+      transition: background-color 100ms ease;
+    }
+
+    &:hover::after {
+      background-color: #1e1e1e;
+    }
+
+    > * {
+    padding: ${rem(12)} 0;
+    display: block;
+  }
+  }
+
+  button {
+    width: 100%;
+    text-align: left;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
