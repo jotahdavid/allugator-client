148287d9e78007289170c18dee03d7c9a859cf37
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-05 09:46:26 -0300

    feat(user): create service

---
+++ src/services/UsersService.ts
@@ -0,0 +1,48 @@
+import HttpClient from '@services/HttpClient';
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface UserCreation extends Omit<User, 'id'> {
+  password: string;
+}
+
+export interface UserCredential {
+  email: string;
+  password: string;
+}
+
+export type UserResponseWithoutToken = Omit<UserResponse, 'token'>;
+
+interface UserResponse {
+  user: User;
+  token: string;
+}
+
+class UsersService {
+  private http = new HttpClient('http://localhost:3001');
+
+  async createUser(user: UserCreation) {
+    const response = await this.http.post<UserResponse, UserCreation>('/users', user);
+    return response.data;
+  }
+
+  async login(credential: UserCredential) {
+    const response = await this.http.post<UserResponse, UserCredential>('/auth/login', credential);
+    return response.data;
+  }
+
+  async getByToken(token: string) {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    const response = await this.http.get<UserResponseWithoutToken>('/users/me', { headers });
+    return response.data;
+  }
+}
+
+export default new UsersService();
