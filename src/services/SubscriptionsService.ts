66e4ddef2f915f26ac333dc9aa20159719e03491
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-11-05 16:00:15 -0300

    feat(subscriptions): create service

---
+++ src/services/SubscriptionsService.ts
@@ -0,0 +1,24 @@
+import HttpClient from '@services/HttpClient';
+import { ProductResponse } from '@services/ProductsService';
+
+interface SubscriptionResponse {
+  id: string;
+  price: number;
+  expiresAt: string;
+  product: ProductResponse
+}
+
+class SubscriptionsService {
+  private http = new HttpClient('http://localhost:3001');
+
+  async listSubscriptionsByToken(token: string) {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    const response = await this.http.get<SubscriptionResponse[]>('/users/me/subscriptions', { headers });
+    return response.data;
+  }
+}
+
+export default new SubscriptionsService();
