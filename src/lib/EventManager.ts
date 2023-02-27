e1e48a3c375d68c0b20d24910463148855aef4b7
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-02-27 19:19:26 -0300

    feat: create EventManager class

---
+++ src/lib/EventManager.ts
@@ -0,0 +1,32 @@
+type EventManagerListener<T> = (payload: T) => void;
+type EventManagerMapListeners<P> = Map<string, EventManagerListener<P>[]>;
+
+class EventManager<P> {
+  public listeners: EventManagerMapListeners<P>;
+
+  constructor() {
+    this.listeners = new Map();
+  }
+
+  on(event: string, listener: EventManagerListener<P>) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
+    }
+    this.listeners.get(event)?.push(listener);
+  }
+
+  emit(event: string, payload: P) {
+    if (!this.listeners.has(event)) return;
+    this.listeners.get(event)?.forEach((listener) => listener(payload));
+  }
+
+  removeListener(event: string, listenerToRemove: EventManagerListener<P>) {
+    if (!this.listeners.has(event)) return;
+    const filteredListeners = this.listeners.get(event)!.filter(
+      (listener) => listener !== listenerToRemove,
+    );
+    this.listeners.set(event, filteredListeners);
+  }
+}
+
+export default EventManager;
